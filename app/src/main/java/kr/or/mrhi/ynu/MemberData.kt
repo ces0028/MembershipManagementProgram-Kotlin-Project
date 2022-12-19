456b4761b67d12002b0eb0c0d3295c3742419d22
ces0028 <ceshin0028@gmail.com>
2022-12-19 12:46:23 +0900

    [CREATE] MemberData.kt

---
+++ app/src/main/java/kr/or/mrhi/ynu/MemberData.kt
@@ -0,0 +1,11 @@
+package kr.or.mrhi.ynu
+
+data class MemberData(
+    val id: String,
+    var password: String,
+    var name: String,
+    var position: String,
+    var department: String,
+    var phone: String,
+    var email: String,
+    var address: String)
